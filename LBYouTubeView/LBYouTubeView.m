aeda0ec03f5aa6421eacbadf998f286313ff1cc9
Dominik Gruber <mail@dominikgruber.com>
2012-06-01 16:29:30 +0200

    Support for youtu.be URLs
    e.g. http://youtu.be/1fTIhC1WSew
    That's the default format YouTube provides when the user hits the "Share" button

--- LBYouTubeView/LBYouTubeView.m
+++ LBYouTubeView/LBYouTubeView.m
@@ -197,7 +197,10 @@ static NSString* const kLBYouTubeViewErrorDomain = @"LBYouTubeViewErrorDomain";
 #pragma mark Other Methods
 
 +(BOOL)URLIsValid:(NSURL *)URL {
-    return [URL.host isEqualToString:@"www.youtube.com"];
+    return ([URL.host isEqualToString:@"www.youtube.com"] 
+            || [URL.host isEqualToString:@"www.youtube.com"]
+            || [URL.host isEqualToString:@"youtu.be"]
+            || [URL.host isEqualToString:@"www.youtu.be"]);
 }
 
 -(void)loadYouTubeVideoWithId:(NSString*)videoId
