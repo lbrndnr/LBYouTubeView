2668098ca6c0d67647a52e674e18d960fa0a587f
Laurin <larcus94@gmail.com>
2012-06-29 22:40:53 +0200

    Cleaned up code

---
+++ LBYouTubeExtractor/LBYouTubePlayerController.h
@@ -0,0 +1,13 @@
+//
+//  LBYouTubePlayerController.h
+//  LBYouTubeView
+//
+//  Created by Laurin Brandner on 29.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBYouTubePlayerController : UIView
+
+@end
---
+++ LBYouTubeExtractor/LBYouTubePlayerController.m
@@ -0,0 +1,31 @@
+//
+//  LBYouTubePlayerController.m
+//  LBYouTubeView
+//
+//  Created by Laurin Brandner on 29.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LBYouTubePlayerController.h"
+
+@implementation LBYouTubePlayerController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
