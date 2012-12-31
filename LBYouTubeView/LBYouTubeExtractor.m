782d2c13427ca187461deaeb5e06e399b47604a3
Laurin <larcus94@gmail.com>
2012-12-31 14:51:03 +0100

    Removed JSONKit.

--- LBYouTubeView/LBYouTubeExtractor.m
+++ LBYouTubeView/LBYouTubeExtractor.m
@@ -7,7 +7,6 @@
 //
 
 #import "LBYouTubeExtractor.h"
-//#import "JSONKit.h"
 
 static NSString* const kUserAgent = @"Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
 
