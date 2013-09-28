a59f3230da40f0bd9d940c1f0d190d31fd77378a
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-09-28 13:32:46 +0200

    Add support for Vevo videos with encrypted signatures

--- LBYouTubeView/LBYouTubeExtractor.m
+++ LBYouTubeView/LBYouTubeExtractor.m
@@ -16,6 +16,8 @@ NSInteger const LBYouTubePlayerExtractorErrorCodeInvalidHTML  =    1;
 NSInteger const LBYouTubePlayerExtractorErrorCodeNoStreamURL  =    2;
 NSInteger const LBYouTubePlayerExtractorErrorCodeNoJSONData   =    3;
 
+static NSString* algoJson = @"[80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 0, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 63]";
+
 @interface LBYouTubeExtractor ()
 
 @property (nonatomic, strong) NSURLConnection* connection;
@@ -37,6 +39,8 @@ NSInteger const LBYouTubePlayerExtractorErrorCodeNoJSONData   =    3;
         self.youTubeURL = videoURL;
         self.quality = videoQuality;
         self.extractionExpression = @"(?!\\\\\")http[^\"]*?itag=[^\"]*?(?=\\\\\")";
+		self.signatureExtractionExpression = @"(?<=sig\\\\\": \\\\\")[^\"]*?(?=\\\\\")";
+		self.signAlgo = [NSJSONSerialization JSONObjectWithData:[algoJson dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
     }
     return self;
 }
@@ -89,25 +93,38 @@ NSInteger const LBYouTubePlayerExtractorErrorCodeNoJSONData   =    3;
     NSString* string = html;
     
     NSRegularExpression* regex = [[NSRegularExpression alloc] initWithPattern:self.extractionExpression options:NSRegularExpressionCaseInsensitive error:error];
+	NSRegularExpression* sig_regex = [[NSRegularExpression alloc] initWithPattern:self.signatureExtractionExpression options:NSRegularExpressionCaseInsensitive error:nil];
     NSArray* videos = [regex matchesInString:string options:0 range:NSMakeRange(0, [string length])];
+	NSArray* sigs = [sig_regex matchesInString:string options:0 range:NSMakeRange(0, [string length])];
     
     if (videos.count > 0) {
         NSTextCheckingResult* checkingResult = nil;
+		NSTextCheckingResult* sigCheckingResult = nil;
+		unsigned int index = 0;
         
         if (self.quality == LBYouTubeVideoQualityLarge) {
-            checkingResult = [videos objectAtIndex:0];
+            index = 0;
         }
         else if (self.quality == LBYouTubeVideoQualityMedium) {
-            unsigned int index = MIN(videos.count-1, 1U);
-            checkingResult= [videos objectAtIndex:index];
+             index = MIN(videos.count-1, 1U);
         }
         else {
-            checkingResult = [videos lastObject];
+			index = [videos count] - 1;
         }
+		checkingResult = [videos objectAtIndex:index];
+		
         
         NSMutableString* streamURL = [NSMutableString stringWithString: [string substringWithRange:checkingResult.range]];
         [streamURL replaceOccurrencesOfString:@"\\\\u0026" withString:@"&" options:NSCaseInsensitiveSearch range:NSMakeRange(0, streamURL.length)];
         [streamURL replaceOccurrencesOfString:@"\\\\\\" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, streamURL.length)];
+
+		// Check the signature:
+		if ([sigs count] > 0) {
+			sigCheckingResult = [sigs objectAtIndex:index];
+			NSString* encrSyg = [string substringWithRange:sigCheckingResult.range];
+			NSString* sig = [self decryptSignature:encrSyg];
+			[streamURL appendString:[@"&signature=" stringByAppendingString:sig]];
+		}
                 
         return [NSURL URLWithString:streamURL];
     }
@@ -115,6 +132,17 @@ NSInteger const LBYouTubePlayerExtractorErrorCodeNoJSONData   =    3;
     *error = [NSError errorWithDomain:kLBYouTubePlayerExtractorErrorDomain code:2 userInfo:[NSDictionary dictionaryWithObject:@"Couldn't find the stream URL." forKey:NSLocalizedDescriptionKey]];
     
     return nil;
+}
+
+-(NSString *)decryptSignature:(NSString *)signature
+{
+	NSMutableString* res = [NSMutableString string];
+	for (int i=0; i<[self.signAlgo count]; i++) {
+		NSNumber* index = [self.signAlgo objectAtIndex:i];
+		NSRange range = {[index integerValue], 1};
+		[res insertString:[signature substringWithRange:range] atIndex:i];
+	}
+	return [NSString stringWithString:res];
 }
 
 -(void)didSuccessfullyExtractYouTubeURL:(NSURL *)videoURL {
