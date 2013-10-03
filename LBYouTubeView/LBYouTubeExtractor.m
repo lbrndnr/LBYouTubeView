a3f0e05fa7543c141d0d952b183802b92333a93b
Michael Rimestad <michael@beolink.com>
2013-10-03 16:56:55 +0200

    Added Ability to decode signature key using the json function from the HTML. Will require IOS7 as javascriptcore framework is used. Another option is to use a web view to call the function.

--- LBYouTubeView/LBYouTubeExtractor.m
+++ LBYouTubeView/LBYouTubeExtractor.m
@@ -7,6 +7,7 @@
 //
 
 #import "LBYouTubeExtractor.h"
+#import <JavaScriptCore/JavaScriptCore.h>
 
 static NSString* const kUserAgent = @"Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
 
@@ -122,7 +123,47 @@ static NSString* algoJson = @"[80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 6
 		if ([sigs count] > 0) {
 			sigCheckingResult = [sigs objectAtIndex:index];
 			NSString* encrSyg = [string substringWithRange:sigCheckingResult.range];
-			NSString* sig = [self decryptSignature:encrSyg];
+            
+            NSString* sig = nil;
+            
+            //Get the javascript function used to decode the encoded key!
+            NSRegularExpression * regexp = [[NSRegularExpression alloc] initWithPattern:@"signature=([a-zA-Z]+)" options:NSRegularExpressionCaseInsensitive error:error];
+            NSRange signature_range = [regexp rangeOfFirstMatchInString:string options:0 range:NSMakeRange(0, [string length])];
+            
+            //If we have a function name refrence, continue trying to pass
+            if(signature_range.location != NSNotFound)
+            {
+                NSString * signature_functionName = [string substringWithRange:NSMakeRange(signature_range.location+10, signature_range.length-10)];
+                //NSLog(@"got signature_functionName = %@", signature_functionName);
+                
+                //We need to extrach the javascript part of the HTML document contaning the function.
+                regexp = [[NSRegularExpression alloc] initWithPattern:[NSString stringWithFormat:@"function %@\\(([a-z,]+)\\)\\{([^}]+)\\}", signature_functionName] options:0 error:error];
+                
+                NSRange function_range = [regexp rangeOfFirstMatchInString:string options:0 range:NSMakeRange(0, [string length])];
+                
+                if(function_range.location != NSNotFound)
+                {
+                    //NSString * jsCode = @"function Gp(a){a=a.split("");a=a.reverse();a=a.slice(1);var b=a[0];a[0]=a[68%a.length];a[68]=b;return a.join("")}";
+                    NSString * jsCode = [string substringWithRange:function_range];
+                    JSContext *context = [[JSContext alloc] initWithVirtualMachine:[[JSVirtualMachine alloc] init]];
+                    
+                    [context evaluateScript:jsCode];
+                    
+                    JSValue * func = context[signature_functionName];
+                    NSArray * args = @[encrSyg];
+                    JSValue * ret =[func callWithArguments:args];
+                    
+                    if([ret isString])
+                        sig = [ret toString];
+                }
+            }
+
+            //if we are unable to grap the json, fall back to static decrypt algo.
+            if(!sig){
+                NSLog(@"Unable to get decoded key from JSon, falling back to static method");
+                sig = [self decryptSignature:encrSyg];
+            }
+            
 			[streamURL appendString:[@"&signature=" stringByAppendingString:sig]];
 		}
                 
@@ -144,6 +185,240 @@ static NSString* algoJson = @"[80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 6
 	}
 	return [NSString stringWithString:res];
 }
+
+-(NSString *)_static_decrypt_signature:(NSString *)signature{
+    //    if age_gate{
+    //        # The videos with age protection use another player, so the
+    //        # algorithms can be different.
+    //        if len(s) == 86:
+    //            return s[2:63] + s[82] + s[64:82] + s[63]
+    //    }
+    NSLog(@"length %d", signature.length);
+    
+    switch (signature.length) {
+        case 93:
+        {
+            return [NSString stringWithFormat:@"%@%@%@",
+                    [self stringRange:signature andStart:86 andStop:29],
+                    [self stringRange:signature charAtIndex:88],
+                    [self stringRange:signature andStart:28 andStop:5]];
+            //return s[86:29:-1] + s[88] + s[28:5:-1]
+        }
+            break;
+        case 92:
+        {
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@",
+                    [self stringRange:signature charAtIndex:25],
+                    [self stringRange:signature andStart:3 andStop:25],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:26 andStop:42],
+                    [self stringRange:signature charAtIndex:79],
+                    [self stringRange:signature andStart:43 andStop:79],
+                    [self stringRange:signature charAtIndex:91],
+                    [self stringRange:signature andStart:80 andStop:83]];
+        }
+            //return s[25] + s[3:25] + s[0] + s[26:42] + s[79] + s[43:79] + s[91] + s[80:83];
+            break;
+        case 91:
+        {
+            return [NSString stringWithFormat:@"%@%@%@",
+                    [self stringRange:signature andStart:84 andStop:27],
+                    [self stringRange:signature charAtIndex:86],
+                    [self stringRange:signature andStart:26 andStop:5]];
+        }
+            //return s[84:27:-1] + s[86] + s[26:5:-1]
+            break;
+        case 90:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@",
+                    [self stringRange:signature charAtIndex:25],
+                    [self stringRange:signature andStart:3 andStop:25],
+                    [self stringRange:signature charAtIndex:2],
+                    [self stringRange:signature andStart:26 andStop:40],
+                    [self stringRange:signature charAtIndex:77],
+                    [self stringRange:signature andStart:41 andStop:77],
+                    [self stringRange:signature charAtIndex:89],
+                    [self stringRange:signature andStart:78 andStop:81]];
+            
+            //return s[25] + s[3:25] + s[2] + s[26:40] + s[77] + s[41:77] + s[89] + s[78:81]
+            break;
+        case 89:
+            return [NSString stringWithFormat:@"%@%@%@%@%@",
+                    [self stringRange:signature andStart:84 andStop:78],
+                    [self stringRange:signature charAtIndex:87],
+                    [self stringRange:signature andStart:77 andStop:60],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:59 andStop:3]];
+            //return s[84:78:-1] + s[87] + s[77:60:-1] + s[0] + s[59:3:-1]
+            break;
+        case 88:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@",
+                    [self stringRange:signature andStart:7 andStop:28],
+                    [self stringRange:signature charAtIndex:87],
+                    [self stringRange:signature andStart:29 andStop:45],
+                    [self stringRange:signature charAtIndex:55],
+                    [self stringRange:signature andStart:46 andStop:55],
+                    [self stringRange:signature charAtIndex:2],
+                    [self stringRange:signature andStart:56 andStop:87],
+                    [self stringRange:signature charAtIndex:28]];
+            //return s[7:28] + s[87] + s[29:45] + s[55] + s[46:55] + s[2] + s[56:87] + s[28]
+            break;
+        case  87:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@",
+                    [self stringRange:signature andStart:6 andStop:27],
+                    [self stringRange:signature charAtIndex:4],
+                    [self stringRange:signature andStart:28 andStop:39],
+                    [self stringRange:signature charAtIndex:27],
+                    [self stringRange:signature andStart:40 andStop:59],
+                    [self stringRange:signature charAtIndex:2],
+                    [self stringRange:signature andStart:60 andStop:87]];
+            //return s[6:27] + s[4] + s[28:39] + s[27] + s[40:59] + s[2] + s[60:]
+            break;
+        case  86:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@",
+                    [self stringRange:signature andStart:80 andStop:72],
+                    [self stringRange:signature charAtIndex:16],
+                    [self stringRange:signature andStart:71 andStop:39],
+                    [self stringRange:signature charAtIndex:72],
+                    [self stringRange:signature andStart:38 andStop:16],
+                    [self stringRange:signature charAtIndex:82],
+                    [self stringRange:signature andStart:15 andStop:-1]];
+            //return s[80:72:-1] + s[16] + s[71:39:-1] + s[72] + s[38:16:-1] + s[82] + s[15::-1]
+            break;
+        case  85:
+            return [NSString stringWithFormat:@"%@%@%@%@%@",
+                    [self stringRange:signature andStart:3 andStop:11],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:12 andStop:55],
+                    [self stringRange:signature charAtIndex:84],
+                    [self stringRange:signature andStart:56 andStop:84]];
+            //return s[3:11] + s[0] + s[12:55] + s[84] + s[56:84]
+            break;
+        case  84:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@",
+                    [self stringRange:signature andStart:78 andStop:70],
+                    [self stringRange:signature charAtIndex:14],
+                    [self stringRange:signature andStart:69 andStop:37],
+                    [self stringRange:signature charAtIndex:70],
+                    [self stringRange:signature andStart:36 andStop:14],
+                    [self stringRange:signature charAtIndex:80],
+                    [self stringRange:signature andStart:13 andStop:-1]];
+            //return s[78:70:-1] + s[14] + s[69:37:-1] + s[70] + s[36:14:-1] + s[80] + s[:14][::-1]
+            break;
+        case  83:
+        {
+            return [NSString stringWithFormat:@"%@%@%@%@",
+                    [self stringRange:signature andStart:80 andStop:63],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:62 andStop:0],
+                    [self stringRange:signature charAtIndex:63]];
+            //return s[80:63:-1] + s[0] + s[62:0:-1] + s[63]
+        }
+            break;
+        case  82:
+            return [NSString stringWithFormat:@"%@%@%@%@",
+                    [self stringRange:signature charAtIndex:12],
+                    [self stringRange:signature andStart:79 andStop:12],
+                    [self stringRange:signature charAtIndex:80],
+                    [self stringRange:signature andStart:11 andStop:-1]];
+            //return s[12] + s[79:12:-1] + s[80] + s[11::-1]
+            break;
+        case  81:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@%@%@",
+                    [self stringRange:signature charAtIndex:56],
+                    [self stringRange:signature andStart:79 andStop:56],
+                    [self stringRange:signature charAtIndex:41],
+                    [self stringRange:signature andStart:55 andStop:41],
+                    [self stringRange:signature charAtIndex:80],
+                    [self stringRange:signature andStart:40 andStop:34],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:33 andStop:29],
+                    [self stringRange:signature charAtIndex:34],
+                    [self stringRange:signature andStart:28 andStop:9],
+                    [self stringRange:signature charAtIndex:29],
+                    [self stringRange:signature andStart:8 andStop:0],
+                    [self stringRange:signature charAtIndex:9]];
+            //return s[56] + s[79:56:-1] + s[41] + s[55:41:-1] + s[80] + s[40:34:-1] + s[0] + s[33:29:-1] + s[34] + s[28:9:-1] + s[29] + s[8:0:-1] + s[9]
+            break;
+        case  80:
+            return [NSString stringWithFormat:@"%@%@%@%@%@",
+                    [self stringRange:signature andStart:1 andStop:19],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:20 andStop:68],
+                    [self stringRange:signature charAtIndex:19],
+                    [self stringRange:signature andStart:69 andStop:80]];
+            //return s[1:19] + s[0] + s[20:68] + s[19] + s[69:80]
+            break;
+        case  79:
+            return [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@%@%@",
+                    [self stringRange:signature charAtIndex:54],
+                    [self stringRange:signature andStart:77 andStop:54],
+                    [self stringRange:signature charAtIndex:39],
+                    [self stringRange:signature andStart:53 andStop:39],
+                    [self stringRange:signature charAtIndex:78],
+                    [self stringRange:signature andStart:38 andStop:34],
+                    [self stringRange:signature charAtIndex:0],
+                    [self stringRange:signature andStart:33 andStop:29],
+                    [self stringRange:signature charAtIndex:34],
+                    [self stringRange:signature andStart:28 andStop:9],
+                    [self stringRange:signature charAtIndex:29],
+                    [self stringRange:signature andStart:8 andStop:0],
+                    [self stringRange:signature charAtIndex:9]];
+            //return s[54] + s[77:54:-1] + s[39] + s[53:39:-1] + s[78] + s[38:34:-1] + s[0] + s[33:29:-1] + s[34] + s[28:9:-1] + s[29] + s[8:0:-1] + s[9]
+            break;
+            
+        default:
+            return nil;
+            break;
+    }
+    
+}
+
+-(NSString *) stringRange:(NSString *)string andStart:(int)start andStop:(int)stop
+{
+    if(stop < start)
+    {
+        NSMutableString *rtr = [NSMutableString new];
+        //        unichar buf[1];
+        
+        if(start <= string.length && stop >= -1)
+        {
+            while (start > stop) {
+                unichar uch = [string characterAtIndex:start--];
+                [rtr appendString:[NSString stringWithCharacters:&uch length:1]];
+            }
+            
+            return rtr;
+        }
+        return [NSString new];
+        
+    }
+    else if(start < string.length && stop <= string.length && stop > start)
+    {
+        NSMutableString *rtr=[NSMutableString stringWithCapacity:stop-start];
+        
+        while (start < stop) {
+            unichar uch = [string characterAtIndex:start++];
+            [rtr appendString:[NSString stringWithCharacters:&uch length:1]];
+        }
+        return rtr;
+    }
+    
+    return [NSString new];
+}
+
+-(NSString *) stringRange:(NSString *)string charAtIndex:(int)index
+{
+    //NSMutableString *rtr=[NSMutableString new];
+    //        unichar buf[1];
+    if(index < string.length)
+    {
+        unichar uch = [string characterAtIndex:index];
+        return [NSString stringWithCharacters:&uch length:1];
+    }
+    return [NSString new];
+}
+
+
 
 -(void)didSuccessfullyExtractYouTubeURL:(NSURL *)videoURL {
     if (self.delegate) {
